304f98dc5148d06fd97bda7dd95780727b1664a1
mamun.abdullah <mamun_abdullah@PC20003.local>
2022-08-17 16:18:34 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect DB
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
+//Initializing express
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+//Middleware dev logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler);
+
+//Set port variable
+const PORT = process.env.PORT || 5000;
+//Initializing app
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  );
+});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
